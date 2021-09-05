934dda7514d5298a65afc4fbfe27a1ee24eb9654
yeganeh <ytaghavi@yahoo.com>
2021-09-04 22:28:47 -0700

    Add recording script

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const RawVid = require('./models/RawVid');
+//const RawVid = require('./models/RawVid');
 
-mongoose.connect('mongodb://localhost:27017/smartly-surf', {
+/*mongoose.connect('mongodb://localhost:27017/smartly-surf', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -17,7 +17,7 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
-});
+});*/
 
 const app = express();
 
@@ -29,10 +29,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 
-
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render("home.ejs")
 });
+
+app.get('/record', (req, res) => {
+    res.render('recording.ejs')
+});
+
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
--- views/layouts/script.js
+++ views/layouts/script.js
@@ -1,30 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SmartylSurf</title>
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
-        integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-</head>
-
-<body class="d-flex flex-column vh-100">
-    <%- include('../partials/navbar')%>
-    <main class="container mt-5">
-        <%- body %>
-    </main>
-    <%- include('../partials/footer')%>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"
-        integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/"
-        crossorigin="anonymous"></script>
-
-    <script>
-        'use strict';
+'use strict';
 
 //TODO: make modifaible in the same page by user
 const oneSecond = 1000;
@@ -168,30 +142,3 @@ async function init(constraints) {
     handleSuccess(stream);
     print(stream)
 }
-
-    </script>
-
-    <script>
-        (function () {
-            'use strict'
-
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            const forms = document.querySelectorAll('.validated-form')
-
-            // Loop over them and prevent submission
-            Array.from(forms)
-                .forEach(function (form) {
-                    form.addEventListener('submit', function (event) {
-                        if (!form.checkValidity()) {
-                            event.preventDefault()
-                            event.stopPropagation()
-                        }
-
-                        form.classList.add('was-validated')
-                    }, false)
-                })
-        })()
-    </script>
-</body>
-
-</html>
